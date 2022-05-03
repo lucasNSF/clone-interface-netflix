6af2bc351b53523934cd17bca69679d87a7cf695
lucasNSF <nascimentomr10@gmail.com>
2022-05-03 19:07:57 -0300

    Clone da Netflix

---
+++ js/owl/main.js
@@ -0,0 +1,16 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: -50,
+  nav: true,
+  responsive: {
+    599: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
